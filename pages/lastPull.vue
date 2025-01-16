390b6ed908016302716000f277833c0b51fe44c0
Numil <1353306+Numil@users.noreply.github.com>
2025-01-16 18:07:07 +0100

    Removed last pull infos

--- pages/lastPull.vue
+++ pages/lastPull.vue
@@ -11,6 +11,6 @@ const lastPullPhase = computed(() => lastPull.value?.lastPhase)
         class="flex flex-col gap-1 bg-blue-200 bg-opacity-40 rounded-tr-2xl px-4 py-2 h-screen w-fit *:drop-shadow-[2px_2px_0px_rgba(0,0,0,0.9)] text-blue-50 font-extrabold uppercase text-[3rem]"
     >
         <span v-if="lastPullPercent">Last pull: {{ lastPullPercent }}</span>
-        <span v-if="lastPullPhase">Phase: {{ lastPullPhase }}</span>
+        <!-- <span v-if="lastPullPhase">Phase: {{ lastPullPhase }}</span> Already shown in top view -->
     </div>
 </template>
